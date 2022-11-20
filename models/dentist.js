c2abc98a1f5f8c06cd9de63ec9b2314ad01186d6
Cyn-Thea <fadzai@net.chalmers.se>
2022-11-20 16:30:33 +0100

    desntist schema

---
+++ models/dentist.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DentistsSchema = new Schema({
+  id: { type: Number,
+    unique: true },
+  name: String,
+  owner: String,
+  numberOfDentists: Number,
+  address: String,
+  city: String,
+  coordinate: {
+    longitude: String,
+    latitude: String,
+  },
+  openinghours: {
+    monday: String,
+    tuesday: String,
+    wednesday: String,
+    thursday: String,
+    friday: String,
+  },
+});
+
+module.exports = mongoose.model("dentists", DentistsSchema);
