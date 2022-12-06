4d7e4721ab7f896948bbb05f0765adc92bd983d2
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2022-12-06 14:33:37 +0100

    dentisthandler commit changes

---
+++ src/DBConnection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Dentists = require('./model/dentist.js');
+
+
+// Variables
+var mongoURI = process.env.MONGODB_URI || 'mongodb+srv://Dentistimo:QsyJymgvpYZZeJPc@cluster0.hnkdpp5.mongodb.net/?retryWrites=true&w=majority'; 
+var port = process.env.PORT || 3000;
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+}); 
+
+
+
+
+module.exports.connect = connect;
